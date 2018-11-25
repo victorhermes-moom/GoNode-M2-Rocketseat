6e0f7d78ad2d66d0180a7a77dac2f5864c36cdf3
Victor Hermes <me.victorhermes@gmail.com>
2018-11-25 21:02:49 -0200

    Login encriptado funcional

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,11 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
+const SessionController = require('./app/controllers/SessionController')
 const UserController = require('./app/controllers/UserController')
+
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
 
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
