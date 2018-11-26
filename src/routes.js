8e00d5ff6eebb78789a46af662d6a9304ad4666e
Victor Hermes <me.victorhermes@gmail.com>
2018-11-26 11:46:59 -0200

    Estilo para agendar horários

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ const FileController = require('./app/controllers/FileController')
 const DashboardController = require('./app/controllers/DashboardController')
 const SessionController = require('./app/controllers/SessionController')
 const UserController = require('./app/controllers/UserController')
+const AppointmentController = require('./app/controllers/AppointmentController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -32,5 +33,7 @@ routes.use('/app', authMiddleware)
 routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashboardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
 
 module.exports = routes
